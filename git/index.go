deb7f8d6d12a50566e9826b95a0e6c1955f970c7
Shawn <shawnisacoder@163.com>
2020-07-01 15:57:03 +0800

    Comment: Add comment about git package

--- git/index.go
+++ git/index.go
@@ -1,3 +1,4 @@
+// 封装git命令及其执行抽象
 package git
 
 import (
@@ -5,8 +6,11 @@ import (
 	"os/exec"
 )
 
+// git命令
 const gitCommand = "git"
 
+// 执行git命令并响应执行结果
+// @param args git命令行参数
 func runGit(args ...string) (string, error) {
 	buffer := new(bytes.Buffer)
 	cmd := exec.Command(gitCommand, args...)
@@ -16,10 +20,14 @@ func runGit(args ...string) (string, error) {
 	return buffer.String(), err
 }
 
+// 执行 git version 命令
 func RunGitVersion() (string, error) {
 	return runGit("version")
 }
 
+// 执行 git alias 命令
+// @param alias 别名
+// @param cmd 原命令名
 func RunGitAlias(alias string, cmd string) error {
 	_, err := runGit("config", "alias."+alias, cmd)
 	return err
